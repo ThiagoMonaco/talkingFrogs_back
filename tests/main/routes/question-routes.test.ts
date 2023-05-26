7d571abcba50e896d86727dc80499861cf1562a0
Thiago Monaco <thiago.monaco@cwi.com.br>
2023-05-26 09:43:49 -0300

    test: add integration test to answer question route

--- tests/main/routes/question-routes.test.ts
+++ tests/main/routes/question-routes.test.ts
@@ -1,9 +1,13 @@
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongo-helper'
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { mockAccountModelWithAccessToken } from '@tests/domain/mocks/account-model-mock'
 import request from 'supertest'
 import app from '@main/config/app'
 import { faker } from '@faker-js/faker'
+import { mockQuestionModel } from '@tests/domain/mocks/question-model-mock'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import env from '@main/config/env'
 
 
 let accountCollection: Collection
@@ -32,6 +36,30 @@ describe('Question routes', () => {
                 .send({
                     accountName: accountParams.name,
                     question: faker.lorem.sentence()
+                }).expect(200)
+        })
+    })
+
+    describe('POST /answer', () => {
+
+        test('should return 200 on answer question', async () => {
+            const accountParams = mockAccountModelWithAccessToken()
+            const question: any = mockQuestionModel()
+            question.answer = null
+            question.questionId = new ObjectId('012345678910')
+
+            accountParams.questions.push(question)
+            const insertResult = await accountCollection.insertOne(accountParams)
+            const accessToken = jwt.sign({ id: insertResult.insertedId }, env.jwtSecret, { expiresIn: env.jwtExpiresIn })
+
+            await accountCollection.updateOne({ _id: insertResult.insertedId }, { $set: { accessToken } })
+
+            await request(app)
+                .post('/api/question/answer')
+                .set('x-access-token', accessToken)
+                .send({
+                    questionId: question.questionId.toString(),
+                    answer: faker.lorem.sentence()
                 }).expect(200)
         })
     })
