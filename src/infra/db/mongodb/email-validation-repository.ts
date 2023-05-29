e61e08041a1843714094ad963e94c4f679af9a5a
Thiago Monaco <thiago.monaco@cwi.com.br>
2023-05-29 15:30:13 -0300

    feat: implement validate-email-token-repository and tests

--- src/infra/db/mongodb/email-validation-repository.ts
+++ src/infra/db/mongodb/email-validation-repository.ts
@@ -1,7 +1,10 @@
 import { GenerateEmailTokenRepository } from '@data/protocols/db/account/generate-email-token-repository'
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongo-helper'
+import { ValidateEmailTokenRepository } from '@data/protocols/db/account/validate-email-token-repository'
 
-export class EmailValidationRepository implements GenerateEmailTokenRepository {
+export class EmailValidationRepository implements
+    GenerateEmailTokenRepository,
+    ValidateEmailTokenRepository {
     async generate(data: GenerateEmailTokenRepository.Params): Promise<GenerateEmailTokenRepository.Result> {
         const emailValidationTokens = MongoHelper.getCollection('emailValidationToken')
         const token = data.uuid.slice(0, 5)
@@ -12,6 +15,16 @@ export class EmailValidationRepository implements GenerateEmailTokenRepository {
         })
 
         return token
+    }
+
+    async validateEmailToken(params: ValidateEmailTokenRepository.Params): Promise<ValidateEmailTokenRepository.Result> {
+        const emailValidationTokens = MongoHelper.getCollection('emailValidationToken')
+        const result = await emailValidationTokens.deleteOne({
+            email: params.accountEmail,
+            token: params.token
+        })
+
+        return result.deletedCount > 0
     }
 
 }
--- tests/infra/db/mongodb/email-validation-repository.spec.ts
+++ tests/infra/db/mongodb/email-validation-repository.spec.ts
@@ -18,19 +18,63 @@ describe('EmailValidationMongoRepository', () => {
         await emailValidationTokensCollection.deleteMany({})
     })
 
-    test('Should return token on success', async () => {
-        const sut = new EmailValidationRepository()
-        const params = {
-            email: faker.internet.email(),
-            uuid: faker.datatype.uuid()
-        }
+    describe('generate()', () => {
+        test('Should return token on success', async () => {
+            const sut = new EmailValidationRepository()
+            const params = {
+                email: faker.internet.email(),
+                uuid: faker.datatype.uuid()
+            }
 
-        const token = await sut.generate(params)
+            const token = await sut.generate(params)
 
-        const emailValidationToken = await emailValidationTokensCollection.findOne({ email: params.email })
+            const emailValidationToken = await emailValidationTokensCollection.findOne({ email: params.email })
 
-        expect(token).toBe(params.uuid.slice(0, 5))
-        expect(emailValidationToken.email).toBe(params.email)
-        expect(emailValidationToken.token).toBe(token)
+            expect(token).toBe(params.uuid.slice(0, 5))
+            expect(emailValidationToken.email).toBe(params.email)
+            expect(emailValidationToken.token).toBe(token)
+        })
     })
+
+    describe('validateEmailToken()', () => {
+        test('Should return true on success', async () => {
+            const sut = new EmailValidationRepository()
+            const params = {
+                accountEmail: faker.internet.email(),
+                token: faker.datatype.uuid()
+            }
+
+            await emailValidationTokensCollection.insertOne({
+                email: params.accountEmail,
+                token: params.token
+            })
+
+            const isValid = await sut.validateEmailToken(params)
+
+            expect(isValid).toBeTruthy()
+            expect(await emailValidationTokensCollection.countDocuments()).toBe(0)
+        })
+
+        test('Should return false if token is invalid', async () => {
+            const sut = new EmailValidationRepository()
+            const params = {
+                accountEmail: faker.internet.email(),
+                token: faker.datatype.uuid()
+            }
+
+            await emailValidationTokensCollection.insertOne({
+                email: params.accountEmail,
+                token: params.token
+            })
+
+            const isValid = await sut.validateEmailToken({
+                accountEmail: params.accountEmail,
+                token: 'wrong_token'
+            })
+
+            expect(isValid).toBeFalsy()
+            expect(await emailValidationTokensCollection.countDocuments()).toBe(1)
+        })
+    })
+
 })
